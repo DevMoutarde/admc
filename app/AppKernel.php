7a9c70622e7bcaafdee80768861afd5901178cd4
DevMoutarde <fourcault.gabin@gmail.com>
2016-06-12 11:06:11 +0200

    ajout fosUserBundle commit avant de mapper les entite

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new ADMC\CoreBundle\ADMCCoreBundle(),
             new ADMC\DSIBundle\ADMCDSIBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
