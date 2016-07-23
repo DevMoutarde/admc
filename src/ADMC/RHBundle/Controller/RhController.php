e4e4755b002c9db3b9ae8fff1e703a1043af7696
guigui35 <guillaume.nardizzi@wanadoo.fr>
2016-07-23 16:27:50 +0200

    rh controller

--- src/ADMC/RHBundle/Controller/RhController.php
+++ src/ADMC/RHBundle/Controller/RhController.php
@@ -6,8 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class RhController extends Controller
 {
-    public function indexAction(){
-        return $this->render('ADMCRHBundle:Rh:index.html.twig');
+    public function indexAction()
+    {
+        return $this->render('ADMCRHBundle:Default:index.html.twig');
     }
     
         public function menuAction(){
@@ -15,12 +16,10 @@ class RhController extends Controller
         $subtitle="Menu RH";
         $list=array(
             array('link'=>$this->get('router')->generate('admcrh_request_list'), 'name'=>'Liste des demandes en cours'),
-            array('link'=>'#', 'name'=>'Création user'),
-            array('link'=>'#', 'name'=>'Modification user')
-            //attribution logiciel MAIS DANS USER
-            //attribution lecteur réseau MAIS DANS USER
+            array('link'=>'#', 'name'=>'Créer utilisateur'),
+            array('link'=>'#', 'name'=>'Modifier utilisateur'),
         );
-        return $this->render('ADMCRHBundle:Rh:menu.html.twig', array('subtitle'=>$subtitle,'menu'=>$list
+        return $this->render('ADMCRHBundle:RH:menu.html.twig', array('subtitle'=>$subtitle,'menu'=>$list
         ));  
     }
 }
