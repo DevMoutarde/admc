253edcfd88a8838ced8faaec08827e8ea1439077
guigui35 <guillaume.nardizzi@wanadoo.fr>
2016-07-23 15:56:28 +0200

    Menu dsi/rh

--- src/ADMC/RHBundle/Controller/RhController.php
+++ src/ADMC/RHBundle/Controller/RhController.php
@@ -9,4 +9,18 @@ class RhController extends Controller
     public function indexAction(){
         return $this->render('ADMCRHBundle:Rh:index.html.twig');
     }
+    
+        public function menuAction(){
+
+        $subtitle="Menu RH";
+        $list=array(
+            array('link'=>$this->get('router')->generate('admcrh_request_list'), 'name'=>'Liste des demandes en cours'),
+            array('link'=>'#', 'name'=>'Création user'),
+            array('link'=>'#', 'name'=>'Modification user')
+            //attribution logiciel MAIS DANS USER
+            //attribution lecteur réseau MAIS DANS USER
+        );
+        return $this->render('ADMCRHBundle:Rh:menu.html.twig', array('subtitle'=>$subtitle,'menu'=>$list
+        ));  
+    }
 }
