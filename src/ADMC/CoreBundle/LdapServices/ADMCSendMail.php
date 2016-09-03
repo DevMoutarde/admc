a0d0a48f386105ffd0629e0171beaf2a93c84278
DevMoutarde <fourcault.gabin@gmail.com>
2016-09-03 14:44:51 +0200

    update comments on sendMail Service

--- src/ADMC/CoreBundle/LdapServices/ADMCSendMail.php
+++ src/ADMC/CoreBundle/LdapServices/ADMCSendMail.php
@@ -2,8 +2,16 @@
 
 namespace ADMC\CoreBundle\LdapServices;
 
+
+/**
+ * Service permettant d'envoyer des mails
+ */
 class ADMCSendMail{
     
+    /**
+     *
+     * @var type SwiftMailer
+     */
     private $mailer;
     
     public function __construct($mailer){
@@ -12,6 +20,15 @@ class ADMCSendMail{
        
     }
     
+    
+    /**
+     * Méthode permettant d'envoyer un email. 
+     * Les paramètres sont: destinataire, sujet, contenu du message
+     * @param string $destinataire
+     * @param string $sujet
+     * @param string $message
+     * @author Gabin Fourcault
+     */
     public function envoyerMail($destinataire, $sujet, $message){
         
         $mailEnvoi  = \Swift_Message::newInstance()
